41859ce0b8574afd606d342adbda36eb17ca96ea
Rishabh Jain <hello@rishabhjain.dev>
2026-01-31 14:37:01 +0530

    Fix GitHub Pages 404 errors - remove missing vite.svg reference

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -13,7 +13,7 @@ import About from './pages/About';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/grandmasters">
       <div className="min-h-screen bg-white">
         <Header />
         <Routes>
